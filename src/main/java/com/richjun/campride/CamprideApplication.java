746761ee3d73a379abe5d8071c29b77350bb6e4c
jun <ggprgrkjh@naver.com>
2024-07-06 18:40:11 +0900

    first commit

---
+++ src/main/java/com/richjun/campride/CamprideApplication.java
@@ -0,0 +1,13 @@
+package com.richjun.campride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamprideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamprideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richjun/campride/CamprideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richjun.campride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamprideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
