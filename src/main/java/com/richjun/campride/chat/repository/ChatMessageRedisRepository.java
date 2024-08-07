065a208e598796be6f17b4f114424386348f6bc5
jun <ggprgrkjh@naver.com>
2024-08-07 15:22:53 +0900

    feat: 채팅 페이징 추가

--- src/main/java/com/richjun/campride/chat/repository/ChatMessageRedisRepository.java
+++ src/main/java/com/richjun/campride/chat/repository/ChatMessageRedisRepository.java
@@ -3,6 +3,7 @@ package com.richjun.campride.chat.repository;
 import java.util.List;
 import java.util.Map;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties.Jedis;
 import org.springframework.data.domain.Range;
 import org.springframework.data.redis.connection.Limit;
@@ -15,7 +16,7 @@ import org.springframework.data.redis.core.StreamOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
 
-
+@Slf4j
 @Repository
 @AllArgsConstructor
 public class ChatMessageRedisRepository {
@@ -33,7 +34,29 @@ public class ChatMessageRedisRepository {
     public List<MapRecord<String, String, Map<String, String>>> getMessages(String roomId, int startOffset,
                                                                             int count) {
         StreamOperations<String, String, Map<String, String>> streamOps = redisTemplate.opsForStream();
-        return streamOps.reverseRange("/room/" + roomId, Range.closed("-", "+"), Limit.limit().count(count));
+
+        String startId = getStreamIdAtOffset(roomId, startOffset * 10);
+
+        log.info("startId: {}", startId);
+        log.info("endId: {}", "-");
+        log.info(String.valueOf(startOffset * 10));
+
+        return streamOps.range("/room/" + roomId, Range.closed(startId, "+"), Limit.limit().count(count));
     }
+
+    private String getStreamIdAtOffset(String roomId, int offset) {
+        StreamOperations<String, String, Map<String, String>> streamOps = redisTemplate.opsForStream();
+        List<MapRecord<String, String, Map<String, String>>> records = streamOps.reverseRange(
+                "/room/" + roomId, Range.open("-", "+"), Limit.limit().count(offset + 1));
+
+        if (records != null && records.size() > 0) {
+            log.info("records: {}", records);
+            return records.get(records.size()-1).getId().getValue();  // Ensuring we don't go out of range
+        } else {
+            log.info("$");
+            return "$";  // If the offset is out of range, use the latest ID
+        }
+    }
+
 
 }
