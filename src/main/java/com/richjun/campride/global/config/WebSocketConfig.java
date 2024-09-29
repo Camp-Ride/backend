70e68679cfbaaaaf4974926aec4e64e575c809f8
jun <ggprgrkjh@naver.com>
2024-09-29 22:02:07 +0900

    feat(websocket): 웹소켓 endpoint wss ws 추가

--- src/main/java/com/richjun/campride/global/config/WebSocketConfig.java
+++ src/main/java/com/richjun/campride/global/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.richjun.campride.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,11 +10,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${websocket.endpoint}")
+    private String endpoint;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
-        registry.addEndpoint("/ws").setAllowedOrigins("*");
+        registry.addEndpoint(endpoint).setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint(endpoint).setAllowedOrigins("*");
 
     }
 
