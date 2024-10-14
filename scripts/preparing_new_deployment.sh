59234fd347e866e9892ad12fdbf58a9c047ecf02
jun <ggprgrkjh@naver.com>
2024-10-15 03:09:05 +0900

    feat(workflow): nginx 파일 권한 추가

--- scripts/preparing_new_deployment.sh
+++ scripts/preparing_new_deployment.sh
@@ -3,3 +3,8 @@
 echo "Preparing for new deployment"
 cd /home/ubuntu/develop/backend
 mkdir -p build/libs_new
+
+# Nginx 설정 파일 및 디렉토리 권한 조정
+sudo chown -R ubuntu:ubuntu /home/ubuntu/develop/backend/data/nginx
+sudo chmod 755 /home/ubuntu/develop/backend/data/nginx
+sudo chmod 644 /home/ubuntu/develop/backend/data/nginx/nginx.conf
