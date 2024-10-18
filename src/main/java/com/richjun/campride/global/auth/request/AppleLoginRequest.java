781044cfb09b7326fe8c398af6c3634cdc380a6c
jun <ggprgrkjh@naver.com>
2024-10-18 18:25:33 +0900

    feat(request): appleLoginrequest 빈 생성자 추가

--- src/main/java/com/richjun/campride/global/auth/request/AppleLoginRequest.java
+++ src/main/java/com/richjun/campride/global/auth/request/AppleLoginRequest.java
@@ -1,15 +1,13 @@
 package com.richjun.campride.global.auth.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppleLoginRequest {
     private String identityToken;
     private String deviceToken;
-
-    public AppleLoginRequest(String identityToken, String deviceToken) {
-        this.identityToken = identityToken;
-        this.deviceToken = deviceToken;
-    }
-
 }
