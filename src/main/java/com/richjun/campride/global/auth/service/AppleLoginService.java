9535c061ea61ed968e644029729476175c1069a9
jun <ggprgrkjh@naver.com>
2024-10-31 13:41:48 +0900

    feat(jwt): 애플 로그인 시 refreshToken 생성되어 있는것 반환

--- src/main/java/com/richjun/campride/global/auth/service/AppleLoginService.java
+++ src/main/java/com/richjun/campride/global/auth/service/AppleLoginService.java
@@ -1,6 +1,7 @@
 package com.richjun.campride.global.auth.service;
 
 import static com.richjun.campride.global.exception.ExceptionCode.FAIL_TO_LOGIN_WITH_APPLE;
+import static com.richjun.campride.global.exception.ExceptionCode.NOT_FOUND_REFRESH_TOKEN;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -9,7 +10,10 @@ import com.richjun.campride.global.auth.request.ApplePublicKey;
 import com.richjun.campride.global.auth.response.ApplePublicKeyResponse;
 import com.richjun.campride.global.auth.util.ApplePublicKeyGenerator;
 import com.richjun.campride.global.exception.AuthException;
+import com.richjun.campride.global.exception.BadRequestException;
 import com.richjun.campride.global.jwt.JwtTokenProvider;
+import com.richjun.campride.global.jwt.domain.RefreshToken;
+import com.richjun.campride.global.jwt.domain.repository.RefreshTokenRepository;
 import com.richjun.campride.global.jwt.dto.TokenAppleLoginResponse;
 import com.richjun.campride.global.jwt.dto.TokenResponse;
 import com.richjun.campride.global.jwt.service.TokenService;
@@ -19,15 +23,18 @@ import io.jsonwebtoken.Claims;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
+import java.time.Instant;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import javax.naming.AuthenticationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class AppleLoginService {
 
@@ -37,15 +44,17 @@ public class AppleLoginService {
     private final TokenService tokenService;
     private final UserService userService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     public AppleLoginService(RestTemplateBuilder restTemplateBuilder, ObjectMapper objectMapper,
                              ApplePublicKeyGenerator applePublicKeyGenerator, TokenService tokenService,
-                             UserService userService, JwtTokenProvider jwtTokenProvider) {
+                             UserService userService, JwtTokenProvider jwtTokenProvider, RefreshTokenRepository refreshTokenRepository) {
         this.restTemplate = restTemplateBuilder.build();
         this.applePublicKeyGenerator = applePublicKeyGenerator;
         this.tokenService = tokenService;
         this.userService = userService;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
     public ApplePublicKeyResponse fetchApplePublicKeys() {
@@ -87,10 +96,14 @@ public class AppleLoginService {
 
             // 7. 자체 토큰 생성
             String accessToken = jwtTokenProvider.generateAccessToken(user.getSocialLoginId(), user.getRole());
-            String refreshToken = jwtTokenProvider.generateRefreshToken(user.getSocialLoginId(), user.getRole());
+
+            RefreshToken refreshToken = refreshTokenRepository.findByUser(user)
+                    .map(this::verifyExpirationAndUpdateToken)
+                    .orElseThrow(() -> new BadRequestException(NOT_FOUND_REFRESH_TOKEN));
+
 
             // 8. 토큰 응답 반환
-            return new TokenAppleLoginResponse(accessToken, refreshToken, user.getIsNicknameUpdated());
+            return new TokenAppleLoginResponse(accessToken, refreshToken.getToken(), user.getIsNicknameUpdated());
         } catch (AuthenticationException e) {
             throw new AuthException(FAIL_TO_LOGIN_WITH_APPLE);
         } catch (NoSuchAlgorithmException e) {
@@ -101,6 +114,15 @@ public class AppleLoginService {
             throw new RuntimeException(e);
         }
 
+    }
+
+    private RefreshToken verifyExpirationAndUpdateToken(RefreshToken refreshToken) {
+        if (refreshToken.getExpiryDate().compareTo(Instant.now()) < 0) {
+            log.info("refreshToken expired : {}", refreshToken.getToken());
+            refreshToken.updateToken();
+            return refreshTokenRepository.save(refreshToken);
+        }
+        return refreshToken;
     }
 
 
